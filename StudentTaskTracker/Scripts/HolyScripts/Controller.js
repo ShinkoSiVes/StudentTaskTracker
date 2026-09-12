a67d6bffc78a0583908deea5c4dac1ee9d2b4b3e
ShinkoSiVes <ShinkoSiVes@users.noreply.github.com>
2026-09-13 06:39:09 +0800

    Week 2 Finished

--- StudentTaskTracker/Scripts/HolyScripts/Controller.js
+++ StudentTaskTracker/Scripts/HolyScripts/Controller.js
@@ -1,6 +1,32 @@
 ﻿app.controller("StudentTaskTrackerController", function ($scope, StudentTaskTrackerService) {
 
-    $scope.nameoffunc = function () {
-        alert("working");
-    }
+    $scope.userArray = [];
+
+    $scope.registrationFunc = function () {
+        // filtering section
+        if (
+            $scope.FName == undefined || $scope.FName == "" ||
+            $scope.MName == undefined || $scope.MName == "" ||
+            $scope.LName == undefined || $scope.LName == ""
+        ) {
+            alert("Please fill in all fields.");
+
+        // input array section
+        } else {
+            var userData = {
+                firstName: $scope.FName,
+                middleName: $scope.MName,
+                lastName: $scope.LName
+            };
+
+            $scope.userArray.push(userData);
+        }
+    };
+
+    $scope.cancelFunc = function () {
+        $scope.FName = "";
+        $scope.MName = "";
+        $scope.LName = "";
+    };
+
 });
