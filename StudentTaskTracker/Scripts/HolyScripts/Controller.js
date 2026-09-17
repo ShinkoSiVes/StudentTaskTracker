d32fa1d17ec55ab9cba5050e6d96bcebe96c098e
ShinkoSiVes <ShinkoSiVes@users.noreply.github.com>
2026-09-17 16:23:25 +0800

    embeded the copyFunc() into the editFunc() and change file directoru

--- StudentTaskTracker/Scripts/HolyScripts/Controller.js
+++ StudentTaskTracker/Scripts/HolyScripts/Controller.js
@@ -1,9 +1,10 @@
-﻿app.controller("StudentTaskTrackerController", function ($scope, StudentTaskTrackerService) {
+﻿app.controller("StudentTaskTrackerController", function($scope, StudentTaskTrackerService) {
 
     $scope.userArray = [];
     var nextId = 1;
+    $scope.editIndex = null;    
 
-    $scope.registrationFunc = function () {
+    $scope.registrationFunc = function() {
         if (
             $scope.UName == undefined || $scope.UName == "" ||
             $scope.FName == undefined || $scope.FName == "" ||
@@ -53,12 +54,11 @@
             };
 
             $scope.userArray.push(userData);
-            $scope.clearFunc();
             $scope.swalsuccessFunc("Registration successful!");
         }
     };
 
-    $scope.clearFunc = function () {
+    $scope.clearFunc = function() {
         $scope.UName = "";
         $scope.FName = "";
         $scope.LName = "";
@@ -69,7 +69,22 @@
         $scope.loginUName = "";
     };
 
-    $scope.editFunc = function (userId) {
+    $scope.editFunc = function (index) {
+        // This was where the copyFunc() is transferred to
+        if ($scope.editIndex !== index) {
+            var userData = $scope.userArray[index];
+            $scope.UName = userData.userName;
+            $scope.FName = userData.firstName;
+            $scope.LName = userData.lastName;
+            $scope.Email = userData.email;
+            $scope.Pass = userData.password;
+            $scope.conPass = userData.confirmationPassword;
+
+            $scope.editIndex = index;
+            $scope.clearFunc();
+            $scope.swalsuccessFunc("Data copied into the form.");
+            return;
+        }
 
         if (
             $scope.UName == undefined || $scope.UName == "" ||
@@ -80,12 +95,7 @@
             $scope.conPass == undefined || $scope.conPass == ""
         ) {
             $scope.swalerrorFunc("Please fill in everything.");
-        }
-        else if (isUsernameTaken($scope.UName, userId)) {
-            $scope.swalerrorFunc("Username is already taken.");
-        }
-        else if (isEmailTaken($scope.Email, userId)) {
-            $scope.swalerrorFunc("Email is already taken.");
+            return;
         }
         else if (
             $scope.UName.length < 3 || $scope.UName.length > 75 ||
@@ -93,43 +103,47 @@
             $scope.LName.length < 2 || $scope.LName.length > 30
         ) {
             $scope.swalerrorFunc("Username must be 3-75 characters, and First/Last Name must be 2-30 characters.");
+            return;
         }
         else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test($scope.Email)) {
             $scope.swalerrorFunc("Please enter a valid email address.");
+            return;
         }
         else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test($scope.Pass)) {
             $scope.swalerrorFunc("Please enter a strong password. (8 characters, uppercase, lowercase, number, and a special character)");
+            return;
         }
         else if ($scope.Pass !== $scope.conPass) {
             $scope.swalerrorFunc("Passwords do not match.");
+            return;
         }
-        else {
-            var userData = $scope.userArray[userId];
-            userData.userName = $scope.UName;
-            userData.firstName = $scope.FName;
-            userData.lastName = $scope.LName;
-            userData.email = $scope.Email;
-            userData.password = $scope.Pass;
-            userData.confirmationPassword = $scope.conPass;
 
-            $scope.clearFunc();
-            $scope.swalsuccessFunc("Record updated successfully!");
-        }
+        var userData = $scope.userArray[$scope.editIndex];
+        userData.userName = $scope.UName;
+        userData.firstName = $scope.FName;
+        userData.lastName = $scope.LName;
+        userData.email = $scope.Email;
+        userData.password = $scope.Pass;
+        userData.confirmationPassword = $scope.conPass;
+
+        $scope.editIndex = null;
+        $scope.clearFunc();
+        $scope.swalsuccessFunc("Record updated successfully!");
     };
 
-    $scope.deleteFunc = function (index) {
+    $scope.deleteFunc = function(index) {
         $scope.delswalFunc(
             "Are you sure?",
             "Yes",
             "No",
-            function () {
+            function() {
                 $scope.userArray.splice(index, 1);
                 $scope.$apply();
             }
         );
     }
 
-    $scope.loginFunc = function () {
+    $scope.loginFunc = function() {
         if (
             $scope.loginUName == undefined || $scope.loginUName == "" ||
             $scope.loginPass == undefined || $scope.loginPass == ""
@@ -141,14 +155,17 @@
         }
     }
 
-    $scope.copyFunc = function (index) {
-        var userData = $scope.userArray[index];
-        $scope.UName = userData.userName;
-        $scope.FName = userData.firstName;
-        $scope.LName = userData.lastName;
-        $scope.Email = userData.email;
-        $scope.swalsuccessFunc("Data copied into the form.");
-    };
+
+
+    //     This is the original editFunc that was commented out. It copies the data from the selected user into the form fields for editing.
+        //     $scope.editFunc = function(index) {
+        //     var userData = $scope.userArray[index];
+        //     $scope.UName = userData.userName;
+        //     $scope.FName = userData.firstName;
+        //     $scope.LName = userData.lastName;
+        //     $scope.Email = userData.email;
+        //     $scope.swalsuccessFunc("Data copied into the form.");
+        // };
 
     function isUsernameTaken(username, excludeId) {
         for (var i = 0; i < $scope.userArray.length; i++) {
